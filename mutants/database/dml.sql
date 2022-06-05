aabec072bcfb74a45145e823ce25270326a30828
Wilson Antonio Panqueba León <wpanqueb@bancolombia.com.co>
2022-06-05 17:07:32 -0500

    incorpotacion de base de datos

---
+++ mutants/database/dml.sql
@@ -0,0 +1,16 @@
+CREATE TABLE public.adn_test
+(
+    is_mutant boolean,
+    adn_md5 character varying(32) COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.adn_test
+    OWNER to postgres;
+    
+CREATE INDEX adn_test_index
+    ON public.adn_test USING btree
+    (adn_md5 COLLATE pg_catalog."default" ASC NULLS LAST, is_mutant DESC NULLS LAST)
+    TABLESPACE pg_default;
+    
